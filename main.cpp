7961249da843ad0ae3362d70cc3761d92d2ff2bd
lorence56 <148061672+lorence56@users.noreply.github.com>
2024-02-05 22:22:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 
-using namespace std;
+bool containsDuplicate(std::vector<int>& nums) {
+    std::unordered_set<int> seen;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    for (int num : nums) {
+        if (seen.find(num) != seen.end()) {
+            // Found a duplicate
+            return true;
+        }
+
+        // Add the element to the set
+        seen.insert(num);
+    }
+
+    // No duplicates found
+    return false;
+}
+
+int main() {
+    std::vector<int> nums = {1, 2, 2, 3, 4, 5, 6, 7};
+
+    if (containsDuplicate(nums)) {
+        std::cout << "The array contains duplicates. True" << std::endl;
+    } else {
+        std::cout << "The array does not contain duplicates. False" << std::endl;
+    }
+
     return 0;
 }
